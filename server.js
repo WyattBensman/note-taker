9de9fb3c9927c86731dc1015fb4b15161b4c9239
WyattBensman <wyattbensman5@gmail.com>
2023-07-31 10:20:42 -0400

    Added app.get api/notes, to get the notes data

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const PORT = 3000;
 
 const path = require('path');
+const fs = require('fs');
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
 // Require Database
 const notes = require('./db/db.json')
 
@@ -18,6 +23,33 @@ app.get('/', (req, rese) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
+
+// API to get all of the notes
+app.get('/api/notes', (req, res) => {
+    // Read the JSON file to get the notes
+    res.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+        // Check if there's an error
+        if (err) {
+            console.error('Error reading notes data:', err);
+            return res.status(500).json({ error: 'Error reading notes data' });
+        }
+
+        let notes = [];
+        // Parse the JSON data into an array
+        try {
+            notes = JSON.parse(data)
+        }
+        // Check if there's an error parsing the notes
+        catch (err) {
+            console.log('Error parsing JSON File:', err);
+            return res.status(500).json({ error: 'Error parsing notes data' });
+        }
+    })
+})
+
+// API to add a new note
+
+// API to delete a note
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
